dad7d4f22314da781edcab76086e1bdcbd8fcb30
David Eastcroft <davideastcroft95@gmail.com>
2020-04-09 15:22:41 +0100

    Adds test for check for ace method.

--- card_game_specs.rb
+++ card_game_specs.rb
@@ -1,15 +1,21 @@
 require("minitest/autorun")
 require("minitest/reporters")
-require_relative("card.rb")
-require_relative("card_game.rb")
+require_relative("./card.rb")
+require_relative("./card_game.rb")
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestCardGame < Minitest::Test
 
+  def setup
+    @card1 = Card.new("spade", 1)
+    @card2 = Card.new("diamond", 5)
+    @card3 = Card.new("club", 9)
+    @card4 = Card.new("heart", 7)
+  end
+
   def test_check_for_ace
-    @card = Card.new(spade, 1)
-    assert_equal(check_for_ace(@card))
+    assert_equal(true, CardGame.check_for_ace(@card1))
   end
 
 end
