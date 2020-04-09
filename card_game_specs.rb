6eb54e2d4f7eeea42381a211a3d9fdf889485620
David Eastcroft <davideastcroft95@gmail.com>
2020-04-09 15:40:20 +0100

    Adds test for cards total method,

--- card_game_specs.rb
+++ card_game_specs.rb
@@ -12,6 +12,7 @@ class TestCardGame < Minitest::Test
     @card2 = Card.new("diamond", 5)
     @card3 = Card.new("club", 9)
     @card4 = Card.new("heart", 7)
+    @cards = [@card1, @card2, @card3, @card4]
   end
 
   def test_check_for_ace
@@ -19,6 +20,12 @@ class TestCardGame < Minitest::Test
     assert_equal(false, CardGame.check_for_ace(@card2))
   end
 
+  def test_highest_card
+    assert_equal(@card3, CardGame.highest_card(@card2, @card3))
+  end
 
+  def test_cards_total
+    assert_equal("You have a total of 22.", CardGame.cards_total(@cards))
+  end
 
 end
